6615892cbfd68936dfc6f1bc09e3a885c6bd5e54
xiaoyu <doctor.oyy@gmail.com>
2021-08-19 20:47:43 +0800

    add 261.cpp

---
+++ leetcode/261.cpp
@@ -0,0 +1,21 @@
+// https://leetcode-cn.com/problems/graph-valid-tree/
+#include<iostream>
+#include<vector>
+#include<cmath>
+// #include<algorithm>
+using namespace std;
+
+// Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+// Output: true
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int> >& edges) {
+      
+    }
+};
+
+int main() {
+  
+  return 0;
+}
